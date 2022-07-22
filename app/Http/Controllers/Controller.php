de9cc3879c7464e855cd2aa4379e558ef83e19b2
fajarwz <fajarwindhuzulfikar@gmail.com>
2022-07-22 21:22:30 +0700

    init

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *    title="My Cool API",
+ *    description="An API of cool stuffs",
+ *    version="1.0.0",
+ * )
+ */
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
